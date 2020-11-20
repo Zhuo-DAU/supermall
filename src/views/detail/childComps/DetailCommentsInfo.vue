980a389e0cc7388220d9b90ab9798cad14f2e000
Vitor Freitas <vitor@simpleisbetterthancomplex.com>
2020-11-20 21:26:49 +0800

    '详情页和购物车页面完成'

---
+++ src/views/detail/childComps/DetailCommentsInfo.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "DetailCommentsInfo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/detail/childComps/DetailGoodsParam.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "DetailGoodsParam"
+}
+</script>
+
+<style scoped>
+
+</style>
